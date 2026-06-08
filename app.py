55c06683a972c3b692df0b254866a771de077d54
Uma-Shettar <uma.n.shettar@gmail.com>
2026-06-08 15:07:36 +0530

    feat: implement piece movement rules excluding check/checkmate

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ def main():
     p.init()
     screen = p.display.set_mode((width, height))
     s = controllers.State()
+    valid_moves = s.get_valid_moves()
+    print(valid_moves, len(valid_moves))
+    move_done = False
     screen.fill(p.Color("grey"))
     clock = p.time.Clock()
     load_images()
@@ -39,20 +42,41 @@ def main():
                     clicks_list = []
 
                 if len(clicks_list) == 2:
-                    if s.board[clicks_list[0][0]][clicks_list[0][1]] != "--":
-                        move = controllers.Move(clicks_list[0], clicks_list[1], s.board)
+                    move = controllers.Move(clicks_list[0], clicks_list[1], s.board)
+                    print("move", move)
+                    if move in valid_moves:
+                        print("play")
                         s.play_move(move)
-                    selected_position, clicks_list = (), []
+                        move_done = True
+                        selected_position, clicks_list = (), []
+                    else:
+                        clicks_list = [selected_position]
             elif event.type == p.KEYDOWN:
                 if event.key == p.K_u:
                     s.Undo()
-        
-        draw_state(screen, s.board, selected_position)
+                    move_done = True
+        if move_done:
+            print("-----------------------\n---------------------------")
+            valid_moves = s.get_valid_moves()
+            print("enter", valid_moves)
+            move_done = False
+
+        possible_moves = []
+        if selected_position != ():
+            r,c = selected_position
+            moves = s.get_piece_moves(r, c, s.board[r][c][1])
+    
+            if moves:
+                for m in moves:
+                    possible_moves.append((m.to_row,m.to_column))
+            #print(possible_moves)
+
+        draw_state(screen, s.board, selected_position, possible_moves)
             
         clock.tick(fps)
         p.display.flip()
 
-def draw_state(screen, board, selected_position = None):
+def draw_state(screen, board, selected_position = None, possible_moves = None):
     for r in range(8):
         for c in range(8):
             if (r + c) % 2 == 0:
@@ -61,11 +85,19 @@ def draw_state(screen, board, selected_position = None):
                 color = p.Color(69, 81, 107)
             p.draw.rect(screen, color, p.Rect(c*square_size, r*square_size, square_size, square_size))
 
+            highlight = p.Surface((square_size, square_size))
+            highlight.set_alpha(120)
+
             if selected_position == (r, c):
-                highlight = p.Surface((square_size, square_size))
-                highlight.set_alpha(120)
                 highlight.fill(p.Color("yellow"))
                 screen.blit(highlight, (c*square_size, r*square_size))
+            
+            highlight_p = p.Surface((square_size, square_size),p.SRCALPHA)
+            p.draw.circle(highlight_p,(188, 103, 194, 180), (square_size // 2, square_size // 2),square_size // 5.5)
+
+            if possible_moves and ((r, c) in possible_moves):
+                #highlight_p.fill(p.Color(112, 185, 186))
+                screen.blit(highlight_p, (c*square_size, r*square_size))
             
             piece = board[r][c]
             if piece != "--":
--- controllers.py
+++ controllers.py
@@ -13,6 +13,8 @@ class State():
         ]
         self.log = []
         self.white_turn = True
+        self.get_moves_function = {'P' : self.get_pawn_moves, 'R': self.get_rook_moves, 'N': self.get_knight_moves,
+                                   'B': self.get_bishop_moves, 'Q': self.get_queen_moves, 'K': self.get_king_moves, '-': self.return_empty}
     
     def play_move(self, move):
         self.board[move.from_row][move.from_column] = "--"
@@ -26,6 +28,146 @@ class State():
             self.board[move.from_row][move.from_column] = move.piece_moved
             self.board[move.to_row][move.to_column] = move.piece_taken
             self.white_turn = not self.white_turn
+    
+    def get_valid_moves(self):  # check validity considerin checks
+        print("valid moves")
+        return self.get_all_moves_possible() # for now ignoring checks
+    
+    def get_all_moves_possible(self):
+        moves = []
+        print("get_all_moves")
+        for r in range(len(self.board)):
+            for c in range(len(self.board)):
+                piece_color = self.board[r][c][0]
+                if (piece_color == 'W' and self.white_turn) or (piece_color == 'B' and not self.white_turn):
+                    piece = self.board[r][c][1]
+                    moves = moves + self.get_piece_moves(r, c, piece) 
+        return moves
+    
+    def get_piece_moves(self, r, c, piece):
+        print("get_piece_moves")
+        return self.get_moves_function[piece](r, c)
+    
+    def get_pawn_moves(self, r, c):
+        print("get_pawn_moves")
+        moves=[]
+        if self.white_turn and self.board[r][c][0] == 'W':
+            if self.board[r-1][c] == "--":
+                print("1")
+                moves.append(Move((r,c),(r-1,c),self.board))
+                if self.board[r-2][c] == "--" and r == 6:
+                    moves.append(Move((r,c),(r-2,c),self.board))
+                    print("2")
+            if c-1>=0:
+                if self.board[r-1][c-1][0] == "B":
+                    moves.append(Move((r,c),(r-1,c-1),self.board))
+            if c+1<=7:
+                if self.board[r-1][c+1][0] == "B":
+                    moves.append(Move((r,c),(r-1,c+1),self.board))
+        elif self.board[r][c][0] == 'B':
+            if self.board[r+1][c] == "--":
+                moves.append(Move((r,c),(r+1,c),self.board))
+                if self.board[r+2][c] == "--" and r == 1:
+                    moves.append(Move((r,c),(r+2,c),self.board))
+            if c-1>=0:
+                if self.board[r+1][c-1][0] == "W":
+                    moves.append(Move((r,c),(r+1,c-1),self.board))
+            if c+1<=7:
+                if self.board[r+1][c+1][0] == "W":
+                    moves.append(Move((r,c),(r+1,c+1),self.board))
+        return moves
+        
+
+    def get_rook_moves(self, r, c):
+        moves = []
+        move_directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        opponent_color = 'B' if self.white_turn else 'W'
+        if self.board[r][c][0] != opponent_color:
+            for md in move_directions:
+                for i in range(1,8):
+                    to_row = r + md[0]*i
+                    to_column = c + md[1]*i
+                    if 0<= to_row <= 7 and 0 <= to_column <= 7:
+                        square = self.board[to_row][to_column]
+                        if square == "--":
+                            moves.append(Move((r,c),(to_row, to_column),self.board))
+                        elif square[0] == opponent_color:
+                            moves.append(Move((r,c),(to_row, to_column),self.board))
+                            break
+                        else:
+                            break
+                    else:
+                        break
+
+            return moves
+
+    def get_knight_moves(self, r, c):
+        moves = []
+        move_directions = [(-2, -1), (-2, 1), (2, -1), (2, 1), (1, -2), (-1, -2), (1, 2), (-1, 2)]
+        opponent_color = 'B' if self.white_turn else 'W'
+        if self.board[r][c][0] != opponent_color:
+            for md in move_directions:
+                to_row = r + md[0]
+                to_column = c + md[1]
+                if 0<= to_row <= 7 and 0 <= to_column <= 7:
+                    square = self.board[to_row][to_column]
+                    if square == "--":
+                        moves.append(Move((r,c),(to_row, to_column),self.board))
+                    elif square[0] == opponent_color:
+                        moves.append(Move((r,c),(to_row, to_column),self.board))
+
+        return moves
+
+    def get_bishop_moves(self, r, c):
+        moves = []
+        move_directions = [(-1, -1), (1, 1), (1, -1), (-1, 1)]
+        opponent_color = 'B' if self.white_turn else 'W'
+        if self.board[r][c][0] != opponent_color:
+            for md in move_directions:
+                for i in range(1,8):
+                    to_row = r + md[0]*i
+                    to_column = c + md[1]*i
+                    if 0<= to_row <= 7 and 0 <= to_column <= 7:
+                        square = self.board[to_row][to_column]
+                        if square == "--":
+                            moves.append(Move((r,c),(to_row, to_column),self.board))
+                        elif square[0] == opponent_color:
+                            moves.append(Move((r,c),(to_row, to_column),self.board))
+                            break
+                        else:
+                            break
+                    else:
+                        break
+
+            return moves
+
+    def get_queen_moves(self, r, c):
+        moves = []
+        opponent_color = 'B' if self.white_turn else 'W'
+        if self.board[r][c][0] != opponent_color:
+            moves += self.get_rook_moves(r, c)
+            moves += self.get_bishop_moves(r, c)
+        return moves
+
+    def get_king_moves(self, r, c):
+        moves = []
+        move_directions = [(-1, -1), (-1, 1), (-1, 0), (0,1), (0, -1), (1, -1), (1, 1), (1, 0)]
+        opponent_color = 'B' if self.white_turn else 'W'
+        if self.board[r][c][0] != opponent_color:
+            for md in move_directions:
+                to_row = r + md[0]
+                to_column = c + md[1]
+                if 0<= to_row <= 7 and 0 <= to_column <= 7:
+                    square = self.board[to_row][to_column]
+                    if square == "--":
+                        moves.append(Move((r,c),(to_row, to_column),self.board))
+                    elif square[0] == opponent_color:
+                        moves.append(Move((r,c),(to_row, to_column),self.board))
+
+        return moves
+    
+    def return_empty(self,r,c):
+        return []
 
 class Move():
     def __init__(self, from_pos, to_pos, board):
@@ -35,4 +177,11 @@ class Move():
         self.to_column = to_pos[1]
         self.piece_taken = board[self.to_row][self.to_column]
         self.piece_moved = board[self.from_row][self.from_column]
+        self.move_id = 1000*self.from_row + 100*self.from_column + 10*self.to_row + 1*self.to_column
+        print(self.move_id)
+    
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.move_id == other.move_id
+        return False
         
